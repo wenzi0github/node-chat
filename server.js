f2a0afe97b24c784f474b99949b16f0c6d4f1f1b
wenzi <baibai194@gmail.com>
2015-03-31 00:39:37 +0800

    node web chat

---
+++ server.js
@@ -0,0 +1,86 @@
+var express = require('express'), //引入express模块
+    app = express(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server);
+
+app.use('/', express.static(__dirname + '/www')); //指定静态HTML文件的位置
+server.listen(80);
+
+var User = {
+    info : {},
+
+    hasExisted : function(string){
+        var info = this.info;
+        for(var key in info){
+            if(key==string || info[key].nickname==string){
+                return key;
+            }
+        }
+        return false;
+    },
+
+    getInfo : function(userid){
+        return this.info[userid];
+    },
+
+    getLength : function(){
+        var num = 0;
+        for(var key in this.info){
+            num++;
+        }
+        return num;
+    }
+}
+/*
+在connection事件的回调函数中，socket表示的是当前连接到服务器的那个客户端。
+所以代码socket.emit('foo')则只有自己收得到这个事件，
+而socket.broadcast.emit('foo')则表示向除自己外的所有人发送该事件，
+另外，上面代码中，io表示服务器整个socket连接，所以代码io.sockets.emit('foo')表示所有人都可以收到该事件。
+*/
+io.on('connection', function (socket) {
+    socket.on("message", function(obj, userid){
+        var date = new Date(),
+            hour = date.getHours(),
+            minute = date.getMinutes(),
+            second = date.getSeconds();
+        hour = hour<10?'0'+hour : hour;
+        minute = minute<10 ? '0'+minute : minute;
+        second = second<10 ? '0'+second : second;
+
+        if(User.hasExisted(userid)){
+            var info = User.getInfo(userid);
+            info.msg = '<div style="font-size:'+obj.size+'; color:'+obj.color+'">'+obj.msg+'</div>';
+            info.time = hour+':'+minute;
+            io.sockets.emit('msg', { status:"success", info:info});
+        }else{
+            io.sockets.emit('msg', { status:"failure" });
+        }
+        
+    });
+
+    socket.on("login", function(obj){
+        if(User.hasExisted(obj.nickname)){
+            socket.emit("nickExisted");
+        }else{
+            obj.uid = socket.id;
+            User.info[socket.id] = obj;
+            socket.emit("loginSuccess", obj.uid);
+
+            var s = {nickname:obj.nickname, user:User.info, len:User.getLength(), type:"login"};
+
+            io.sockets.emit("system", s);
+        }
+    });
+
+    socket.on("disconnect", function(){
+        var uid = socket.id;
+        if(User.getInfo(uid)){
+            var nickname = User.info[uid].nickname;
+            delete User.info[uid];
+            socket.broadcast.emit('system', {user:{userid:uid, nickname:nickname}, type:"logout"})
+        }else{
+
+        }
+    })
+});
+
---
+++ www/js/wchat.js
@@ -0,0 +1,176 @@
+var socket = io.connect(null);
+socket.on("connect", function(){
+    $("#content").find('.info').html('connect success').hide(function(){
+        $("#content").find('.nickform').show();
+    });
+});
+
+socket.on("nickExisted", function(){
+    $('#tips').html('用户名已经存在');
+})
+socket.on("loginSuccess", function(userid){
+    Chat.userid = userid;
+    $(".layer").hide();
+    $("#msg").focus();
+});
+
+socket.on("system", function(obj){
+    if(obj.type=='login'){
+        var html = '',
+            user = obj.user;
+
+        $(".record").find('.list').append('<div class="notice">'+obj.nickname+' 已加入</div>');
+        for(var key in user){
+            html += '<li><a href="javascript:;" class="clearfix" userid="'+user[key].uid+'"><img src="'+user[key].img+'" alt="pic">'+user[key].nickname+'</a></li>';
+        }
+        $(".user").find('ul').html(html);
+        $("#num").html(obj.len);
+    }else if(obj.type==='logout'){
+        $(".record").find('.list').append('<div class="notice">'+obj.user.nickname+' 已退出</div>');
+        $(".user").find('a[userid='+obj.user.userid+']').parent().remove();
+        $("#num").html(parseInt($("#num").html())-1);
+    }
+    Chat.scroll();
+});
+
+socket.on("msg", function(result){
+    Chat.appendMsg(result);
+})
+
+$(function(){
+    $("#send").click(function(){
+        Chat.send($("#msg").val());
+    });
+
+    $(".portrait").delegate("li", "click", function(){
+        var $this = $(this);
+        $(this).addClass("selected").siblings().removeClass("selected");
+    })
+
+    $("#msg").keydown(function(e){
+        var code = e.charCode || e.which || e.keyCode;
+        var msg = $("#msg").val();
+        
+        if(e.ctrlKey && code == 13) { 
+            $("#msg").val(msg+"\r\n");
+        }else if(!e.ctrlKey && code == 13){
+            Chat.send(msg);
+        } 
+    });
+
+    $(".font span").on("click", function(){
+        var $this = $(this);
+        if($this.hasClass( 'selected' )){
+            $(".font .fontlist").fadeOut();
+            $this.removeClass( 'selected' );
+        }else{
+            $(".font .fontlist").fadeIn();
+            $this.addClass( 'selected' );
+        }
+    });
+
+    $("#fileupload").on("change", function(){
+        //检查是否有文件被选中
+        var $this = $(this),
+            files = $this[0].files;
+        if (files.length != 0) {
+            //获取文件并用FileReader进行读取
+            var file = files[0],
+                reader = new FileReader();
+            if (!reader) {
+                Chat.warning("您的浏览器不支持FileReader");
+                $this.val('');
+                return;
+            };
+            reader.onload = function(e) {
+                //读取成功，显示到页面并发送到服务器
+                $this.val('');
+                socket.emit('message', {msg:'<img src="'+e.target.result+'" alt="img">', color:$('#fontcolor').val(), size:$("#fontsize").val()}, Chat.userid);
+                // that._displayImage('me', e.target.result);
+            };
+            reader.readAsDataURL(file);
+        };
+    });
+
+    $("#fontsize").on("change", function(){
+        $('.chat .send textarea').css({"font-size":$(this).val()});
+    })
+});
+
+window.onbeforeunload = function(event){
+    event = event || window.event;
+    
+    var msg = "是否要离开？";
+    window.event.returnValue = msg;
+    return msg;
+}
+
+var Chat = {
+    // 聊天内容
+    content : '<div class="item clearfix">'+
+                '<div class="pic fl">'+
+                    '<img src="" alt="pic">'+
+                '</div>'+
+                '<div class="info_msg">'+
+                    '<div class="info"><span class="name"></span><span class="timer"></span></div>'+
+                    '<div class="msg"></div>'+
+                '</div>'+
+            '</div>',
+    $tips : $('<div class="tips clearfix"><div><em>!</em><span></span></div></div>'),
+
+    userid : null, // 当前用户
+
+    warning : function(string){
+        this.$tips.find('span').html(string);
+        $(".record").find('.list').append(this.$tips);
+    },
+
+    // 发送消息
+    send : function(){
+        var msg = $.trim($("#msg").val());
+        if(msg===""){
+            return;
+        }
+        if(this.userid==null){
+            this.warning("您还没有登陆");
+        }else{
+            socket.emit("message", {msg:msg, color:$('#fontcolor').val(), size:$("#fontsize").val()}, this.userid);
+        }
+    },
+
+    // 接收消息
+    appendMsg : function(result){
+        if(result.status=="success"){
+            var info = result.info;
+            var $content = $(this.content);
+
+            this.userid==info.uid && $content.addClass( 'louzhu' );
+            $content.find('img').attr('src', info.img);
+            $content.find('.name').html(info.nickname);
+            $content.find('.timer').html(info.time);
+            $content.find('.msg').html(info.msg);
+
+            $(".record").find('.list').append($content);
+            this.scroll();
+            $("#msg").val("").focus();
+        }else{
+            this.warning("认证失败");
+        }
+    },
+
+    // 滚动条永远在最下
+    scroll : function(){
+        $(".record").scrollTop($(".record").find('.list').height());
+    },
+
+    // 登陆
+    login : function(){
+        var nickname = $("#nickname").val();
+        if(nickname===""){
+            return;
+        }
+        var $img = $(".portrait .selected").find("img");
+        socket.emit("login", {nickname:nickname, img:$img.attr("src")});
+        return false;
+    }
+}
